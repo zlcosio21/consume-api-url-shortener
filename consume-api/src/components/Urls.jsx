c3c06d035926705f4b9afe376d0fc6ea55d833eb
zlcosio21 <sebas60621@gmail.com>
2024-04-28 01:20:21 -0500

    Changing structure html of the component Urls

--- consume-api/src/components/Urls.jsx
+++ consume-api/src/components/Urls.jsx
@@ -20,7 +20,7 @@ function Urls() {
     }
 
     useEffect(() => {
-        fetchApi();
+        fetchApi()
     }, [])
 
     return (
@@ -29,73 +29,73 @@ function Urls() {
                 {urls.map(url => (
 
                     <div className='card' key={url.id}>
-                        <div class="card__border"></div>
-                        <div class="card_title__container">
-                            <span class="card_title">API URL CONTENT</span>
+                        <div className="card__border"></div>
+                        <div className="card_title__container">
+                            <span className="card_title">API URL CONTENT</span>
                         </div>
-                        <ul class="card__list">
-                            <li class="card__list_item">
-                                <span class="check">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="check_svg" >
-                                        <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd"></path>
+                        <ul className="card__list">
+                            <li className="card__list_item">
+                                <span className="check">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="check_svg" >
+                                        <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd"></path>
                                     </svg>
                                 </span>
-                                <span class="list_text">ID: {url.id}</span>
+                                <span className="list_text">ID: {url.id}</span>
                             </li>
-                            <li class="card__list_item">
-                                <span class="check">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="check_svg" >
-                                        <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd"></path>
+                            <li className="card__list_item">
+                                <span className="check">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="check_svg url_complete_svg" >
+                                        <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd"></path>
                                     </svg>
                                 </span>
-                                <span class="list_text">Complete Url:&nbsp;
+                                <span className="list_text">Complete Url:&nbsp;
                                     <a href={url.complete_url}>
                                         {url.complete_url}
                                     </a>
                                 </span>
                             </li>
-                            <li class="card__list_item">
-                                <span class="check">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="check_svg" >
-                                        <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd"></path>
+                            <li className="card__list_item">
+                                <span className="check">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="check_svg" >
+                                        <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd"></path>
                                     </svg>
                                 </span>
-                                <span class="list_text">Short Url:&nbsp;
+                                <span className="list_text">Short Url:&nbsp;
                                     <a href={domainUrl + url.short_url}>
                                         {url.short_url}
                                     </a>
                                 </span>
                             </li>
-                            <li class="card__list_item">
-                                <span class="check">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="check_svg" >
-                                        <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd"></path>
+                            <li className="card__list_item">
+                                <span className="check">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="check_svg" >
+                                        <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd"></path>
                                     </svg>
                                 </span>
-                                <span class="list_text">Visits: {url.visits}</span>
+                                <span className="list_text">Visits: {url.visits}</span>
                             </li>
-                            <li class="card__list_item">
-                                <span class="check">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="check_svg" >
-                                        <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd"></path>
+                            <li className="card__list_item">
+                                <span className="check">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="check_svg" >
+                                        <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd"></path>
                                     </svg>
                                 </span>
-                                <span class="list_text">Created: {moment(url.created).format('YYYY-MM-DD HH:mm:ss')}</span>
+                                <span className="list_text">Created: {moment(url.created).format('YYYY-MM-DD HH:mm:ss')}</span>
                             </li>
-                            <li class="card__list_item">
-                                <span class="check">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="check_svg" >
-                                        <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd"></path>
+                            <li className="card__list_item">
+                                <span className="check">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="check_svg" >
+                                        <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd"></path>
                                     </svg>
                                 </span>
-                                <span class="list_text">Domain:&nbsp;
+                                <span className="list_text">Domain:&nbsp;
                                     <a href={domainUrl}>
                                         {domainUrl}
                                     </a>
                                 </span>
                             </li>
                         </ul>
-                        <button class="button">
+                        <button className="button">
                             <a className='link' href={domainUrl + url.short_url}>
                                 GO TO THE COMPLETE URL
                             </a>
